2631d9a48618f69943a9cc688f6d917ee0bf64e2
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2023-01-21 12:39:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config()
+
+
+var indexRouter = require('./routes/index');
+
+
+let saltKey = process.env.SECRET_KEY
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+module.exports = app;
