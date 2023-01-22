ff81c8e029857b91bdaf7b1df227acc4fda1215b
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2023-01-22 13:50:48 +0100

    WIP

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var logger = require('morgan');
 var crypto = require('crypto');
 require('dotenv').config();
 
-
-
-// var indexRouter = require('./routes/index');
+const fs = require('fs');
+const { log } = require('console');
 
 let saltKey = process.env.SECRET_KEY;
 
@@ -24,48 +23,42 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+// let highscoreArray = [
+//     {
+//         username: "kalle",
+//         highscore: 100,
+//     },
+//     {
+//         username: "musse",
+//         highscore: 200,
+//     },
+//     {
+//         username: "Janne",
+//         highscore: 300,
+//     }
+// ]
 
 
-
-
-
-let highscoreArray = [
-    {
-        username: "kalle",
-        highscore: 100,
-    },
-    {
-        username: "musse",
-        highscore: 200,
-    },
-    {
-        username: "Janne",
-        highscore: 300,
-    }
-]
-
-
-let usersArray = [
-    {
-        username: "kalle",
-        password: "anka",
-        highscore: 0,
-        id: crypto.randomUUID()
-    },
-    {
-        username: "musse",
-        password: "pigg",
-        highscore: 0,
-        id: crypto.randomUUID()
-    },
-    {
-        username: "Janne",
-        password: "Långben",
-        highscore: 0,
-        id: crypto.randomUUID()
-    }
-]
+// let usersArray = [
+//     {
+//         username: "kalle",
+//         password: "anka",
+//         highscore: 0,
+//         id: crypto.randomUUID()
+//     },
+//     {
+//         username: "musse",
+//         password: "pigg",
+//         highscore: 0,
+//         id: crypto.randomUUID()
+//     },
+//     {
+//         username: "Janne",
+//         password: "Långben",
+//         highscore: 0,
+//         id: crypto.randomUUID()
+//     }
+// ]
 
 
 
@@ -77,56 +70,97 @@ let usersArray = [
 // get leaderboard
 app.get("/leaderboard", (req, res) => {
 
-    let highscoreArray = ["hej", "hejdå", "vi ses"]
-    res.json(highscoreArray)
+    fs.readFile("./highscores.json", "utf-8", (err, jsonString) => {
+        let highscoreArray = JSON.parse(jsonString);
+        res.json(highscoreArray)
+    })
 })
 
 
 // post highscore to leaderboard
 app.post("/postHighscore/:id", (req, res) => {
 
-    for (let i = 0; i < usersArray.length; i++) {
-        const user = usersArray[i];
+    let highscoreArray = []
 
-        if (user.id === req.params.id) {
+    let usersArray = []
 
-            for (let i = 0; i < highscoreArray.length; i++) {
-                const highscore = highscoreArray[i];
+    fs.readFile("./highscores.json", "utf-8", (err, jsonString) => {
+        highscoreArray = JSON.parse(jsonString);
+        console.log(highscoreArray);
 
-                if (highscore.username === req.body.username && highscore.highscore < req.body.highscore) {
-                    highscore.highscore = req.body.highscore;
-                    let status = { success: "new highscore saved" }
+        fs.readFile("./users.json", "utf-8", (err, jsonString) => {
+            usersArray = JSON.parse(jsonString);
+            console.log(usersArray);
+
+            for (let i = 0; i < usersArray.length; i++) {
+                const user = usersArray[i];
+                console.log(user);
+
+                if (user.id === req.params.id) {
+
+                    for (let i = 0; i < highscoreArray.length; i++) {
+                        const highscore = highscoreArray[i];
+
+                        if (highscore.username === req.body.username && Number(highscore.highscore) < Number(req.body.highscore)) {
+                            highscore.highscore = req.body.highscore;
+
+                            highscoreArray.sort((h1, h2) => (Number(h1.highscore) < Number(h2.highscore)) ? 1 : (Number(h1.highscore) > Number(h2.highscore)) ? -1 : 0);
+
+
+                            let highscoreArraySerialized = JSON.stringify(highscoreArray)
+
+                            fs.writeFile("./highscores.json", highscoreArraySerialized, err => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("file written");
+                                }
+                            })
+
+                            let status = { success: "new highscore saved" }
+                            res.send(status)
+                            return
+                        }
+                        if (highscore.username === req.body.username && Number(highscore.highscore) > Number(req.body.highscore)) {
+                            let status = { error: "new highscore lower than previously saved" }
+                            res.send(status)
+                            return
+                        }
+                    }
+
+                    let newHighscore = {
+                        username: req.body.username,
+                        highscore: req.body.highscore,
+                    }
+
+                    highscoreArray.push(newHighscore)
+
+                    highscoreArray.sort((h1, h2) => (Number(h1.highscore) < Number(h2.highscore)) ? 1 : (Number(h1.highscore) > Number(h2.highscore)) ? -1 : 0);
+
+
+                    let highscoreArraySerialized = JSON.stringify(highscoreArray)
+
+                    fs.writeFile("./highscores.json", highscoreArraySerialized, err => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("file written");
+                        }
+                    })
+
+                    let status = {
+                        success: "new highscore saved",
+                        leaderboard: highscoreArray
+                    }
                     res.send(status)
                     return
                 }
-                if (highscore.username === req.body.username && highscore.highscore > req.body.highscore) {
-                    let status = { error: "new highscore lower than previously saved" }
-                    res.send(status)
-                    return
-                }
+
             }
-
-            let newHighscore = {
-                username: req.body.username,
-                highscore: req.body.highscore,
-            }
-
-            highscoreArray.push(newHighscore)
-
-            highscoreArray.sort((h1, h2) => (h1.highscore < h2.highscore) ? 1 : (h1.highscore > h2.highscore) ? -1 : 0);
-
-
-            let status = {
-                success: "new highscore saved",
-                leaderboard: highscoreArray
-            }
+            let status = { error: "no user" }
             res.send(status)
-            return
-        }
-
-    }
-    let status = { error: "no user" }
-    res.send(status)
+        })
+    })
 })
 
 
